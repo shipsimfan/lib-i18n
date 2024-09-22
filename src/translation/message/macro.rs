163c7a141ea225a18400831a5dd0373adca69ae6
Lance Hart <me@lancehart.ca>
2024-09-21 20:08:39 -0400

    Generalized message macro

--- src/translation/message/macro.rs
+++ src/translation/message/macro.rs
@@ -4,7 +4,7 @@ macro_rules! message {
     ($arguments: ty, |$args: ident, $f: ident| $message: expr) => {{
         $crate::translation::Message::new(|$args, $f| $message)
     }};
-    ($arguments: ty, $message: literal) => {{
+    ($arguments: ty, $message: expr) => {{
         $crate::translation::Message::new(|_, f| ::core::fmt::Display::fmt(&$message, f))
     }};
     ($arguments: ty, $message: path) => {
