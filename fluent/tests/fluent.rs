da611904662456e6b97e92dbd5094d2912e60d0b
Lance Hart <me@lancehart.ca>
2024-11-09 20:03:10 -0500

    Updated i18n

--- fluent/tests/fluent.rs
+++ fluent/tests/fluent.rs
@@ -12,7 +12,7 @@ macro_rules! tests {
                 const SOURCE: &str = include_str!(concat!("fluent/", stringify!($name), ".ftl"));
 
                 let target = fluent::$name::create_target();
-                let resource = ::fluent::parse(SOURCE);
+                let resource = ::i18n_fluent::parse(SOURCE);
                 assert_eq!(target, resource);
             }
         )*
